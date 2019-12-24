cb56c815b5debddd8b808bf545624631b9c33633
Borreguin <rg.sanchez.a@gmail.com>
2019-12-24 08:39:06 -0500

    Include Swagger

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DNP3_API
 {
@@ -24,6 +26,7 @@ namespace DNP3_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -31,8 +34,24 @@ namespace DNP3_API
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.1",
+                    Title = "DNP3 API",
+                    Description = "API for setting DNP3 devices",
+                    TermsOfService = "OpenDNP3 - Apache license",
+                    Contact = new Contact() { Name = "Roberto Sánchez", Email = "rg.sanchez.a@gmail.com", Url = "https://github.com/Borreguin" }
+                });
+                c.DescribeAllEnumsAsStrings();
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(GetXmlCommentsPath());
+            });
+            //services.AddSwaggerGen(SwaggerHelper.ConfigureSwaggerGen);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,12 +71,24 @@ namespace DNP3_API
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            /*app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            });*/
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DNP3 API");
+            }
+            );
+        }
+
+        private string GetXmlCommentsPath()
+        {
+            var app = Directory.GetCurrentDirectory();
+            return System.IO.Path.Combine(app, "DNP3_API.xml");
         }
     }
 }
